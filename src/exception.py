dd9edb194e433dc12c7ee09566a302c3317306f5
unknown <arunasish.v@gmail.com>
2023-07-29 18:24:13 +0530

    EDA and prob statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
+'''
 import sys
-import logging
+import os
+from src.logger import logging
+# import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -16,3 +19,43 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+    try:
+        a=1/10
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
+
+'''
+
+import sys
+# import logging
+from logger import logging
+
+# Assuming you have a logger instance configured already, otherwise configure it here.
+# logger = logging.getLogger("my_logger")
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0  # This will raise a ZeroDivisionError
+#     except Exception as e:
+#         logging.error("Divide by zero")  # Use logging.error() to log the error
+#         raise CustomException(e, sys.exc_info())  # Pass sys.exc_info() instead of sys
