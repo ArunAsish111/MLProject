a4ecc98cc3b08596d6dfdc7faa7960043acc1c02
unknown <arunasish.v@gmail.com>
2023-10-12 20:18:16 +0530

    Deploymnet Configuration AWS

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application=Flask(__name__)
 
@@ -22,11 +22,11 @@ def predict_datapoint():
     else:
         data=CustomData(
             gender=request.form.get('gender'),
-            race_ethinicity=request.form.get('ethinicity'),
+            race_ethnicity=request.form.get('race_ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('writing_score')),
             reading_score=float(request.form.get('reading_score')),
         )
         pred_df=data.get_data_as_data_frame()
